6e76762d469cd4a4fa97e314fb6fafa063d8dd44
Isaac Obuya <Szack4477@gmail.com>
2020-06-27 07:48:36 +0300

    create a recyclerviewer displaying a list of card based items using LinearLayoutManager

--- app/src/main/java/com/example/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.widget.AdapterView;
@@ -41,27 +43,47 @@ public class NoteListActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mAdapterNotes.notifyDataSetChanged();
+//        mAdapterNotes.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
-        final ListView listNotes = findViewById(R.id.list_notes);
+        // for ListView
+//        final ListView listNotes = findViewById(R.id.list_notes);
+//
+//        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+//        mAdapterNotes = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, notes);
+//
+//        listNotes.setAdapter(mAdapterNotes);
+//
+//        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+////                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+//                intent.putExtra(NoteActivity.NOTE_POSITION, position);
+//                startActivity(intent);
+//            }
+//        });
 
-        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        mAdapterNotes = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, notes);
-
-        listNotes.setAdapter(mAdapterNotes);
-
-        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-                intent.putExtra(NoteActivity.NOTE_POSITION, position);
-                startActivity(intent);
-            }
-        });
+        /** now we want to use RecyclerView which provide more customization options
+        *it uses a different kind of adapter and uses Layout Manager to control how Views ara arranged within the RecyclerView
+         * Adapter holds the data and is responsible for creating new view instances and populating each view with its data
+         * steps
+         * a.) Get a reference to the RecyclerView that was loaded with our layout resource.
+         * b.) create an instance of LayoutManager. RecyclerView needs LayoutManager to control arrangement of items within it.
+         *  in this case, we use a LinearLayoutManager which display items in a linear fashion just like ListView but that is more flexible and customizable
+         *  other implementations of LayoutManagers include  GridLayoutManager and StaggeredGridLayoutManager
+         * c.) associate the LinearLayoutManager with the RecyclerView
+         * d.) Design an Item View - need a separate layout resource for this ( want to create a Card appearance of the Item View Layout).
+         * - there is no implicit relationship between this View and and that for list_notes. We can however give our recyclerview a hint of what each item will
+         * look like.
+         **/
+        final RecyclerView recyclerNotes = findViewById(R.id.list_notes);
+        // layout manager
+        final LinearLayoutManager noteLayoutManager = new LinearLayoutManager(this);
+        // associate LinearLayoutManager with the RecyclerView
+        recyclerNotes.setLayoutManager(noteLayoutManager);
 
     }
 
